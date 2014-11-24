72b47bac12ecfb42eb49fe1e27bad932496f24b6
simoncrabtree <simon@crabsticks.net>
2014-11-24 13:32:34 +0000

    Display Hello World using React

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,1 +1,12 @@
-console.log("Hello World");
+var React = require('react');
+
+var HelloWorld = React.createClass({
+	render: function () {
+		return <div>Hello World</div>
+	}
+});
+
+React.renderComponent(
+	<HelloWorld />,
+	document.getElementById('application')
+)
