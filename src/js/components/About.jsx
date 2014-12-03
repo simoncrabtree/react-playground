482a146cf1b787e210ad765ae67f1a604cfe15f0
simoncrabtree <simon@crabsticks.net>
2014-12-03 15:06:51 +0000

    Added react-bootstrap

--- src/js/components/About.jsx
+++ src/js/components/About.jsx
@@ -1,9 +1,13 @@
 var React = require('react');
+var Alert = require('react-bootstrap').Alert;
 
 module.exports = React.createClass({
-  render: function () {
-    return (
-      <div>About</div>
-    )
-  }
+	render: function () {
+		return (
+			<div>
+			<div>About</div>
+			<Alert>Hello</Alert>
+			</div>
+			)
+	}
 });
